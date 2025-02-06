362de06136b42793d718b8b8f8ad726801da1e04
pasyanos <pasyanos.c@gmail.com>
2025-02-06 18:45:45 -0500

    got writing render texture to image working correctly.

--- Assets/Scripts/SpritePipelineController.cs
+++ Assets/Scripts/SpritePipelineController.cs
@@ -4,15 +4,23 @@ public class SpritePipelineController : MonoBehaviour
 {
     // todo: make this a list of cameras, corresponding to the number of perspective types
     [SerializeField] private Camera _sideRenderCamera;
-    [SerializeField] private Texture2D _testTexture;
+    // [SerializeField] private Texture2D _testTexture;
 
-    private void Start()
+
+    private void Start() {}
+
+    private void Update()
     {
-        // RenderTextureToFileUtil.SaveTextureToFile(_testTexture, "Assets/TestRender2");
-        RenderTexture rt = _sideRenderCamera.targetTexture;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.LogError("Hi");
 
-        RenderTextureToFileUtil.SaveRenderTextureToFile(rt, "Assets/TestRenderTexture", RenderTextureToFileUtil.SaveTextureFileFormat.PNG);
+            // RenderTextureToFileUtil.SaveTextureToFile(_testTexture, "Assets/TestRender2");
+            RenderTexture rt = _sideRenderCamera.targetTexture;
 
-        Debug.LogError("Wrote texture");
+            RenderTextureToFileUtil.SaveRenderTextureToFile(rt, "Assets/TestRenderTexture", RenderTextureToFileUtil.SaveTextureFileFormat.PNG);
+
+            Debug.LogError("Wrote texture");
+        }
     }
 }
